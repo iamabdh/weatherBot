3d6a3d6f358336797d672fbf15a026315944ee90
Abdullah Salim <abdullahalbadri09@gmail.com>
2020-09-28 13:30:56 +0400

    Demo

---
+++ weatherBot.py
@@ -0,0 +1,38 @@
+import os
+import discord
+from discord.ext import commands
+import requests
+
+
+bot = commands.Bot(command_prefix='')
+
+@bot.command()
+async def temp_help(ctx):
+    await ctx.send(f'command: `temp city country` '
+                   f'\nexample: `temp muscat om` '
+                   f'\nfor more info about the bot type command `temp_bot`')
+
+
+
+@bot.command()
+async def temp_bot(ctx):
+    await ctx.send(f'created by <@562541986595340289>'
+                   f'\nlanguage: python'
+                   f'\napi: <https://openweathermap.org/current>'
+                   f'\nsource code: <https://github.com/iamabdh>')
+
+
+@bot.command()
+async def temp(ctx, city, cont="om"):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city},{cont}&appid=37a4b1c8693077c739c87c94f38fdd8c'
+    response = requests.get(url)
+    x = response.json()
+
+    check = response.status_code
+    if check == 200:
+        await ctx.send(f"it's { round(x['main']['temp'] - 273.15)}°C in {x['name']}")
+    else:
+        await ctx.send(f'“__please enter valid city and country name__” '
+                       f'\nfor more help type command `temp_help`')
+
+bot.run(os.environ['discord_token'])
